4ff543040eb465075a2f06ad7d63a78c1e8039cf
Jackson Swenson <swensonj@fire.ecs.baylor.edu>
2017-03-02 16:23:21 -0600

    compass working

--- tracker.py
+++ tracker.py
@@ -2,7 +2,9 @@
 from pygame import draw,display
 from pygame.locals import *
 from time import sleep
-from math import sin,cos
+from math import sin,cos,radians
+#switch from pygame
+#import pyglet
 
 class Tracker:
     def __init__(self,motion_controller=None,rotation=0,location=(0,0)):
@@ -10,19 +12,21 @@ class Tracker:
         self.location = location
         self.graph = list()
 
-        self.width = 640
-        self.height = 480
-        self.window = None
+        self.map_width = 640
+        self.map_height = 480
+        self.map_window = None
+
+        self.compass_width = 120 
+        self.compass_height = 120 
+        self.compass_radius = 50
+        self.compass_center = (self.compass_radius + 5, self.compass_radius + 5)
+        self.compass_window = None
 
         self.moving = False
         self.speed = -1
         self.max_speed = 10
         self.speedometer_radius = 10
         self.speedometer_center = (11,11)
-
-        self.compass_radius = 50
-        self.compass_center = (self.width - self.compass_radius - 10, \
-                self.compass_radius + 10)
 
         display.init()
 
@@ -42,32 +46,37 @@ class Tracker:
         self.update_speed()
 
     def show_rotation(self):
-        self.init_window()
+        self.compass_window = display.set_mode((self.compass_width, self.compass_height))
         self.update_rotation()
 
     def update_path(self):
         for p in self.graph:
             draw.circle(self.window,(255,0,0),p,1)
         draw.lines(self.window, (0,0,255), False, self.graph)
-        display.flip()
 
     def update_speed(self):
-        display.flip()
+        print "update_speed not implemented"
 
     def update_rotation(self):
+
+        self.compass_window.fill((0,0,0))
         self.rotation = self.rotation % 360
-        draw.circle(self.window,(0,255,0),self.compass_center, \
+        draw.circle(self.compass_window,(0,255,0),self.compass_center, \
                 self.compass_radius, 1) 
-        point = (self.compass_radius * cos(self.rotation) + self.compass_center[0], \
-                self.compass_radius * sin(self.rotation) + self.compass_center[1])
-        draw.line(self.window, (0,255,0), self.compass_center, point, 2)
-        display.flip()
+        point = (self.compass_radius * cos(radians(self.rotation - 90)) + self.compass_center[0], \
+                self.compass_radius * sin(radians(self.rotation - 90)) + self.compass_center[1])
+        draw.line(self.compass_window, (0,255,0), self.compass_center, point, 2)
 
 if __name__ == "__main__":
     
     t = Tracker()
-    t.graph = ((100,100),(200,100),(200,200),(100,200))
-    t.show_path()
-    t.rotation = 180
+    #t.graph = ((100,100),(200,100),(200,200),(100,200))
+    #t.show_path()
     t.show_rotation()
-        
+    display.flip()
+    for r in range(0,361,45):
+        t.compass_window.fill((0,0,0))
+        t.rotation = r
+        t.update_rotation()
+        display.flip()
+        sleep(1)
