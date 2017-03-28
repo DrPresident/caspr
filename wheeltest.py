fc2aba4047361f843f770b54e2b2c9686412ab77
drpresident <dr@president>
2017-03-28 06:24:05 +0000

    wheeltest working

--- wheeltest.py
+++ wheeltest.py
@@ -7,13 +7,24 @@ io.setup(20,io.OUT)
 io.setup(19,io.OUT)
 io.setup(26,io.OUT)
 
-def forward():
+def rotate_left():
+    io.output(21,True)
+    io.output(20,False)
+    io.output(19,False)
+    io.output(26,True)
+def rotate_right():
+    io.output(21,False)
+    io.output(20,True)
+    io.output(19,True)
+    io.output(26,False)
+
+def back():
 	io.output(21,True)
 	io.output(20,False)
 	io.output(19,True)
 	io.output(26,False)
 
-def back():
+def forward():
 	io.output(21,False)
 	io.output(20,True)
 	io.output(19,False)
